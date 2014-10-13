5371c7091c3faf38939b3b7bb8ccea189455da83
Arron Mabrey <arron@mabreys.com>
2014-10-13 13:38:21 -0400

    Add QuickLook::Instance

--- lib/quick_look.rb
+++ lib/quick_look.rb
@@ -1,5 +1,41 @@
 require "quick_look/version"
 
 module QuickLook
-  # Your code goes here...
+  class Instance
+    def initialize
+      @pid_map = {}
+    end
+
+    def open(path)
+      return if pid_for(path)
+      add_pid path, Process.spawn("qlmanage -p #{path} >& /dev/null", pgroup: true)
+    end
+
+    def close(path)
+      if pid = pid_for(path)
+        Process.kill("-HUP", pid)
+      end
+    end
+
+    private
+
+    def add_pid(path, pid)
+      @pid_map[path] = pid
+    end
+
+    def pid_for(path)
+      pid = @pid_map[path]
+      if pid && !pid_alive?(pid)
+        pid = nil
+        @pid_map.delete(path)
+      end
+      pid
+    end
+
+    def pid_alive?(pid)
+      !!Process.getpgid(pid)
+    rescue Errno::ESRCH # pid is dead
+      false
+    end
+  end
 end
